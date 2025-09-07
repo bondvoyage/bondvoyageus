1b9c0fb0f6b1bd6e29933f27ec80667b7dcda5a4
Grace Yang <graceyang465@gmail.com>
2025-09-06 21:30:21 -0400

    Create articles.js

---
+++ articles.js
@@ -0,0 +1,18 @@
+const client = contentful.createClient({
+  space: 'wrr72jjvus6o',
+  accessToken: 'PmMbpzFsJtzPgmqkKg0au7UMhsyialtZxYulNwZXI74'
+});
+
+client.getEntries({ content_type: 'article' })
+  .then((response) => {
+    const container = document.getElementById('articles');
+    response.items.forEach((item) => {
+      const articleDiv = document.createElement('div');
+      articleDiv.innerHTML = `
+        <h2>${item.fields.title}</h2>
+        <p>${item.fields.body}</p>
+      `;
+      container.appendChild(articleDiv);
+    });
+  })
+  .catch(console.error);
