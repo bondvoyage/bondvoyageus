30bf1fd4373045184a4ae030c929065066d38f2c
Grace Yang <graceyang465@gmail.com>
2025-09-06 21:33:08 -0400

    Update articles.js

--- articles.js
+++ articles.js
@@ -1,18 +1,21 @@
 const client = contentful.createClient({
-  space: 'wrr72jjvus6o',
-  accessToken: 'PmMbpzFsJtzPgmqkKg0au7UMhsyialtZxYulNwZXI74'
+  space: 'YOUR_SPACE_ID',
+  accessToken: 'YOUR_ACCESS_TOKEN'
 });
 
-client.getEntries({ content_type: 'article' })
-  .then((response) => {
-    const container = document.getElementById('articles');
-    response.items.forEach((item) => {
-      const articleDiv = document.createElement('div');
-      articleDiv.innerHTML = `
-        <h2>${item.fields.title}</h2>
-        <p>${item.fields.body}</p>
-      `;
-      container.appendChild(articleDiv);
-    });
-  })
-  .catch(console.error);
+client.getEntries({
+  content_type: 'article',
+  'fields.category': 'Stocks'
+})
+.then((response) => {
+  const container = document.getElementById('articles');
+  response.items.forEach((item) => {
+    const articleDiv = document.createElement('div');
+    articleDiv.innerHTML = `
+      <h2>${item.fields.title}</h2>
+      <p>${item.fields.body}</p>
+    `;
+    container.appendChild(articleDiv);
+  });
+})
+.catch(console.error);
